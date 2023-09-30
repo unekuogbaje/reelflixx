e5919319033fc2e6dcc6d31fee6917f34c010982
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-09-30 17:25:54 +0100

    minor updates

--- src/components/add-to-favourites.jsx
+++ src/components/add-to-favourites.jsx
@@ -8,7 +8,7 @@ const AddToFavourites = () => {
         xmlns="http://www.w3.org/2000/svg" 
         width="16" 
         height="16" 
-        fill="currentColor" 
+        fill="red" 
         class="bi bi-heart-fill" 
         viewBox="0 0 16 16">
         <path 
